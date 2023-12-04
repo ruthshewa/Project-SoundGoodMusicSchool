367410b6aa2302cf369c6828801f7716f17da138
watermelonstrawbery <151861576+watermelonstrawbery@users.noreply.github.com>
2023-12-04 13:28:27 +0100

    Delete Task three/query4.sql

--- Task three/query4.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) as total, lesson_type, 
-TO_CHAR(lesson.date_of_taken_lesson, 'MONTH') AS month_
-FROM lesson
-GROUP BY  TO_CHAR(lesson.date_of_taken_lesson, 'MONTH'),lesson_type
-ORDER BY month_;
